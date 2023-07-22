5d0178320cc19d831bded2cb1c49fc1a316dae7f
Soumik Rakshit <soumik@sostronk.com>
2023-07-22 13:42:41 +0530

    Apply a 3-wide median filter

--- cmd/palette_transfer/palette_transfer.cpp
+++ cmd/palette_transfer/palette_transfer.cpp
@@ -1,17 +1,76 @@
 #define STB_IMAGE_IMPLEMENTATION 1
 #define STB_IMAGE_WRITE_IMPLEMENTATION 1
 
+#include "median_filter.h"
+
 #include <domcolor/argparse.hpp>
 #include <domcolor/dkm_parallel.hpp>
 #include <domcolor/domcolor.h>
 #include <domcolor/stb_image.h>
-#include <filesystem>
 #include <plog/Appenders/ColorConsoleAppender.h>
 #include <plog/Formatters/TxtFormatter.h>
 #include <plog/Init.h>
 #include <plog/Log.h>
 
+#include <filesystem>
+#include <optional>
+
 namespace fs = std::filesystem;
+
+template <int32_t filter_size>
+auto apply_gaussian_filter(const LoadedImage &source) -> std::optional<LoadedImage> {
+  
+}
+
+template <int32_t filter_size>
+auto apply_median_filter(const LoadedImage &source) -> std::optional<LoadedImage>
+{
+  // W - F + 2P + 1
+  // We want the output image to be the same dimension. Then
+  //      W - F + 2P + 1 = W
+  //      P = (F - 1) / 2
+
+  if (filter_size % 2 == 0) {
+    return {};
+  }
+
+  int32_t dest_width = source.width - (filter_size - 1);
+  int32_t dest_height = source.height - (filter_size - 1);
+
+  LoadedImage dest = source;
+
+  int32_t odd_half_size = (filter_size - 1) / 2;
+
+#pragma omp parallel
+  for (int32_t y = odd_half_size; y < dest_height; y++) {
+    for (int32_t x = odd_half_size; x < dest_width; x++) {
+      auto pixels = std::array<Color, filter_size * filter_size>{};
+      auto intensities = std::array<f64, filter_size * filter_size>{};
+      auto indices = std::array<int32_t, filter_size * filter_size>{};
+
+      int32_t i = 0;
+      for (int32_t x_diff = -odd_half_size; x_diff <= odd_half_size; x_diff++) {
+        for (int32_t y_diff = -odd_half_size; y_diff <= odd_half_size; y_diff++) {
+          auto sample_x = x + x_diff;
+          auto sample_y = y + y_diff;
+          auto sample_index = sample_y * source.width + sample_x;
+          const auto pixel = source.pixels[sample_index];
+          pixels[i] = pixel;
+          intensities[i] = (0.2126 * pixel[0]) + (0.7152 * pixel[1]) + (0.0722 * pixel[2]);
+          indices[i] = i;
+          i++;
+        }
+      }
+
+      std::sort(
+          indices.begin(), indices.end(), [&](auto i, auto j) { return intensities[i] < intensities[j]; });
+
+      dest.pixels[y * source.width + x] = pixels[indices[odd_half_size]];
+    }
+  }
+
+  return dest;
+}
 
 auto load_image(fs::path file) -> LoadedImage
 {
@@ -99,7 +158,9 @@ int main(int ac, const char **av)
     }
   }
 
-  destination_image.write_to_file(fs::path(ap.get<std::string>("output-file")));
+  // Apply median filter
+  auto median_filter_output = apply_median_filter<3>(destination_image);
+  median_filter_output.value().write_to_file(fs::path(ap.get<std::string>("output-file")));
 
   PLOGI.printf("DONE creating paletted image");
 }
